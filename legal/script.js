1c27cb9bf5ae48d9cf6406c152a153019cca187e
Arpan-Mandal <92933926+Arpan-Mandal@users.noreply.github.com>
2023-02-17 12:33:22 +0530

    Add files via upload

---
+++ legal/script.js
@@ -0,0 +1,10 @@
+//pussynigga
+
+import { scroll, animate } from "https://cdn.skypack.dev/motion@10.13.1"
+
+scroll(
+	animate("#progress", { scaleX: [0, 1] })
+);
+
+<!--<p><img src="http://source.unsplash.com/PX_1j-M59I8" alt="" title="" width="1080" height="720" class="revealing-image" id="revealing-image-1" /><br /><a href="https://unsplash.com/photos/PX_1j-M59I8">Polar bear walking</a> by <a href="https://unsplash.com/@dtbosse">Daniel Bosse</a></p>--!>
+<!--<p><img src="http://source.unsplash.com/Fk4Y_VkUlSQ" alt="" title="" width="1080" height="720" class="revealing-image" id="revealing-image-2" /><br /><a href="https://unsplash.com/photos/Fk4Y_VkUlSQ">Polar bear shaking off water</a> by <a href="https://unsplash.com/@dtbosse">Daniel Bosse</a></p>
